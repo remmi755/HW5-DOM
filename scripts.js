dcece4690ff6e7f215aef63597af1f8b9a7bc293
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-11-10 13:29:35 +0300

    add homework

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+
+function outputForm () {
+   let userAdd = localStorage.setItem('username', document.getElementById("p1").value);
+   let userShow =  localStorage.getItem('username')
+   let passwordAdd = localStorage.setItem('password', document.getElementById("p2").value) ;
+   let passwordShow = localStorage.getItem('password')
+   let result = document.getElementById('str')
+   return  result.innerHTML =`Вы ввели: username: ${userShow}, password:${passwordShow}`;
+}
+
+addEventListener('click', function(click) {
+
+   click.preventDefault();
+});
+
+let p3 = document.getElementById("p3")
+p3.addEventListener("click",outputForm);
+
+
+setTimeout(function (){
+    p3.removeEventListener("click", outputForm);
+    },30000);
+
+
