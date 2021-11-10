d53213f788490dcb31002c76531f5c03b085b098
remmi755 <90457721+remmi755@users.noreply.github.com>
2021-11-10 18:29:32 +0300

    add new homework

--- scripts.js
+++ scripts.js
@@ -1,21 +1,20 @@
 
 
 function outputForm () {
-   let userAdd = localStorage.setItem('username', document.getElementById("p1").value);
-   let userShow =  localStorage.getItem('username')
-   let passwordAdd = localStorage.setItem('password', document.getElementById("p2").value) ;
-   let passwordShow = localStorage.getItem('password')
-   let result = document.getElementById('str')
-   return  result.innerHTML =`Вы ввели: username: ${userShow}, password:${passwordShow}`;
+let user, passWord, form;
+   user = document.getElementById('p1').value;
+   passWord = document.getElementById('p2').value;
+   localStorage.setItem('username', user);
+   localStorage.setItem('password', passWord);
+   form = document.getElementById('form');
+   form.onsubmit = function(evt) {
+      evt.preventDefault();
+   };
+   document.getElementById('str').innerHTML = `Вы ввели: username: ${user}, password:${passWord}`;
+
 }
 
-addEventListener('click', function(click) {
-
-   click.preventDefault();
-});
-
-let p3 = document.getElementById("p3")
-p3.addEventListener("click",outputForm);
+document.getElementById("p3").addEventListener("click",outputForm);
 
 
 
